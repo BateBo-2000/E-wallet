e7c7586932123cedc61f34355c51d102b4cf3b3e
Boyan <boiansinilkov@abv.bg>
2023-04-03 14:17:24 +0300

    added add balance button

--- client/src/components/ListBalance.js
+++ client/src/components/ListBalance.js
@@ -1,4 +1,5 @@
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
+
 const ListBalance = () => {
     const balances = [
         {
@@ -12,8 +13,44 @@ const ListBalance = () => {
             "user_id": 35,
             "balance": 256.05,
             "currency_id": 5
+          },
+          {
+            "balance_id": 19,
+            "user_id": 35,
+            "balance": 256.05,
+            "currency_id": 5
+          },
+          {
+            "balance_id": 20,
+            "user_id": 35,
+            "balance": 256.05,
+            "currency_id": 5
+          },
+          {
+            "balance_id": 21,
+            "user_id": 35,
+            "balance": 256.05,
+            "currency_id": 5
+          },
+          {
+            "balance_id": 22,
+            "user_id": 35,
+            "balance": 256.05,
+            "currency_id": 5
+          },
+          {
+            "balance_id": 23,
+            "user_id": 35,
+            "balance": 256.05,
+            "currency_id": 5
           }
     ]
+
+    const history = useHistory()
+    const handleClick = () => {
+        history.push('/balance/create')
+    }
+
     return ( 
         <div className="balances">
             {balances.map((balance) => (
@@ -32,6 +69,11 @@ const ListBalance = () => {
                     </Link>
                 </div>
             ))}
+            <div className="balanceElement" key="add-balance">
+                <div className="submit-button-wrap">
+                    <button className="submit-button" onClick={handleClick}>+</button>
+                </div>
+            </div>
         </div> 
     );
 }
